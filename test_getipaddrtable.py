a20e308668fde239b81427bc8bf8c5a112474450
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2022-05-02 21:26:05 +0900

    no comment

--- test_getipaddrtable.py
+++ test_getipaddrtable.py
@@ -3,13 +3,13 @@ import socket
 import struct
 
 dwSize = ULONG(0)
-if GetIpAddrTable(None, byref(dwSize), False) != WIN32_ERROR_ERROR_INSUFFICIENT_BUFFER:
-    raise RuntimeError("GetIpAddrTable")
+if (r := GetIpAddrTable(None, dwSize, False)) != WIN32_ERROR_ERROR_INSUFFICIENT_BUFFER:
+    raise WinError(r)
 
 IpAddrTable = MIB_IPADDRTABLE()
 resize(IpAddrTable, dwSize.value)
-if GetIpAddrTable(byref(IpAddrTable), byref(dwSize), False) != WIN32_ERROR_NO_ERROR:
-    raise RuntimeError("GetIpAddrTable")
+if (r := GetIpAddrTable(IpAddrTable, dwSize, False)) != WIN32_ERROR_NO_ERROR:
+    raise WinError(r)
 
 for row in cast(byref(IpAddrTable.table), POINTER(MIB_IPADDRROW_XP * IpAddrTable.dwNumEntries)).contents:
     print(socket.inet_ntoa(struct.pack("L", row.dwAddr)))
--- test_shortcut.py
+++ test_shortcut.py
@@ -4,18 +4,18 @@ from pathlib import Path
 CoInitialize(None)
 
 shell_link = IShellLinkW()
-if FAILED(CoCreateInstance(ShellLink, None, CLSCTX_CLSCTX_INPROC_SERVER, IShellLinkW.Guid, shell_link)):
-    raise RuntimeException("CoCreateInstance")
+if FAILED(hr := CoCreateInstance(ShellLink, None, CLSCTX_CLSCTX_INPROC_SERVER, IShellLinkW.Guid, shell_link)):
+    raise WinError(hr)
 
 persist_file = IPersistFile()
-if FAILED(shell_link.QueryInterface(IPersistFile.Guid, persist_file)):
-    raise RuntimeException("QueryInterface")
+if FAILED(hr := shell_link.QueryInterface(IPersistFile.Guid, persist_file)):
+    raise WinError(hr)
 
-if FAILED(shell_link.SetPath(str(Path("txttxt.txt").resolve()))):
-    raise RuntimeException("IShellLinkW.SetPath")
+if FAILED(hr := shell_link.SetPath(str(Path("txttxt.txt").resolve()))):
+    raise WinError(hr)
 
-if FAILED(persist_file.Save(str(Path("lnklnk.lnk").resolve()), True)):
-    raise RuntimeException("IPersistFile.Save")
+if FAILED(hr := persist_file.Save(str(Path("lnklnk.lnk").resolve()), True)):
+    raise WinError(hr)
 
 if Path("lnklnk.lnk").exists():
     print("lnklnk.lnk was created")
