a712052d600748c2fd6c7bed23e4b8c504e9cd1b
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-04-19 22:48:47 +0900

    Added unittest

---
+++ tests/test_marshalling.py
@@ -0,0 +1,72 @@
+import unittest
+
+from Windows import (
+    EasyCastStructure,
+    EasyCastUnion,
+    c_char_p_no,
+    c_void_p,
+    c_wchar_p_no,
+    press,
+)
+
+
+class TestMarshalling(unittest.TestCase):
+    def test_c_void_p(self):
+        @press
+        class S(EasyCastStructure):
+            c_void_p: c_void_p
+
+        s = S()
+
+        self.assertIsNone(s.c_void_p)
+
+        s.c_void_p = 0
+        self.assertIsNone(s.c_void_p)
+
+        s.c_void_p = 1234
+        self.assertIsInstance(s.c_void_p, int)
+        self.assertEqual(s.c_void_p, 1234)
+
+    def test_c_char_p(self):
+        @press
+        class S(EasyCastStructure):
+            c_char_p: c_char_p_no
+
+        s = S()
+
+        self.assertIsNone(s.c_char_p)
+
+        s.c_char_p = 0
+        self.assertIsNone(s.c_char_p)
+
+        s.c_char_p = b"abcdefg"
+        self.assertIsInstance(s.c_char_p, bytes)
+        self.assertEqual(s.c_char_p, b"abcdefg")
+
+        s.c_char_p = 1234
+        self.assertIsInstance(s.c_char_p_as_intptr, int)
+        self.assertEqual(s.c_char_p_as_intptr, 1234)
+
+    def test_c_wchar_p(self):
+        @press
+        class S(EasyCastStructure):
+            c_wchar_p: c_wchar_p_no
+
+        s = S()
+
+        self.assertIsNone(s.c_wchar_p)
+
+        s.c_wchar_p = 0
+        self.assertIsNone(s.c_wchar_p)
+
+        s.c_wchar_p = "abcdefg"
+        self.assertIsInstance(s.c_wchar_p, str)
+        self.assertEqual(s.c_wchar_p, "abcdefg")
+
+        s.c_wchar_p = 1234
+        self.assertIsInstance(s.c_wchar_p_as_intptr, int)
+        self.assertEqual(s.c_wchar_p_as_intptr, 1234)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_vaargs.py
+++ tests/test_vaargs.py
@@ -1,8 +1,16 @@
+import unittest
 
-from ctypes import create_unicode_buffer
+from Windows import Char
 from Windows.Win32.UI.Shell import wnsprintfW
 
-buf = create_unicode_buffer(1024)
-r = wnsprintfW(buf, len(buf), "hello %s %d %p", "world", 42, 0x1234)
-print(r, buf.value)
 
+class TestVaArgs(unittest.TestCase):
+    def test_vaargs(self):
+        buf = (Char * 1024)()
+        r = wnsprintfW(buf, len(buf), "hello %s %d %llx", "world", 42, 0x1234)
+        self.assertEqual(len(buf.value), r)
+        self.assertEqual(buf.value, "hello world 42 1234")
+
+
+if __name__ == "__main__":
+    unittest.main()
