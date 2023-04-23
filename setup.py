d2abe35de224a4622244b6591299027f6e4828c0
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-04-23 19:57:15 +0900

    Added pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-import generate
-
-generate.build('json/Windows.Win32.json.xz')
-setup(
-    name="win32more",
-    version="0.4",
-    packages=find_packages()
-)
