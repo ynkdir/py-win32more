aacb06a521cf5273a9c3ac178d5342fd783bfdc3
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-04-26 21:22:02 +0900

    Changed to use ctypes.HRESULT to raise OSError for error value.

--- example/shortcut.py
+++ example/shortcut.py
@@ -1,7 +1,6 @@
 from ctypes import WinError
 from pathlib import Path
 
-from Windows import FAILED
 from Windows.Win32.System.Com import (CLSCTX_INPROC_SERVER, CoCreateInstance,
                                       CoInitialize, CoUninitialize,
                                       IPersistFile)
@@ -10,18 +9,14 @@ from Windows.Win32.UI.Shell import IShellLinkW, ShellLink
 CoInitialize(None)
 
 shell_link = IShellLinkW()
-if FAILED(hr := CoCreateInstance(ShellLink, None, CLSCTX_INPROC_SERVER, IShellLinkW.Guid, shell_link)):
-    raise WinError(hr)
+CoCreateInstance(ShellLink, None, CLSCTX_INPROC_SERVER, IShellLinkW.Guid, shell_link)
 
 persist_file = IPersistFile()
-if FAILED(hr := shell_link.QueryInterface(IPersistFile.Guid, persist_file)):
-    raise WinError(hr)
+shell_link.QueryInterface(IPersistFile.Guid, persist_file)
 
-if FAILED(hr := shell_link.SetPath(str(Path("txttxt.txt").resolve()))):
-    raise WinError(hr)
+shell_link.SetPath(str(Path("txttxt.txt").resolve()))
 
-if FAILED(hr := persist_file.Save(str(Path("lnklnk.lnk").resolve()), True)):
-    raise WinError(hr)
+persist_file.Save(str(Path("lnklnk.lnk").resolve()), True)
 
 if Path("lnklnk.lnk").exists():
     print("lnklnk.lnk was created")
