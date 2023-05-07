3a8305e2cc3f48075a6198b02f1dde54c2f38bff
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-05-08 01:02:02 +0900

    no comment

--- scripts/download_python_for_winrt.ps1
+++ scripts/download_python_for_winrt.ps1
@@ -4,41 +4,25 @@
 
 # Download embeddable python package.
 
-if (-not (Test-Path python)) {
-    New-Item python -ItemType Directory
-}
+Invoke-WebRequest https://www.python.org/ftp/python/3.11.3/python-3.11.3-embed-amd64.zip -OutFile python-3.11.3-embed-amd64.zip
 
-curl.exe -O https://www.python.org/ftp/python/3.11.3/python-3.11.3-embed-amd64.zip
-
-tar.exe -C python -xzvf python-3.11.3-embed-amd64.zip
+Expand-Archive python-3.11.3-embed-amd64.zip -DestinationPath python
 
 # Edit python.exe's manifest for WinRT xaml api.
 
 mt.exe -inputresource:"python\python.exe;#1" -out:python.manifest
 
-(Get-Content -Encoding Ascii python.manifest) | % {
-    if ($_ -like "*{e2011457-1546-43c5-a5fe-008deee3d3f0}*") {
-        '<maxversiontested Id="10.0.18362.0"/>', $_
-    } else {
-        $_
-    }
-} | Set-Content -Encoding Ascii python.manifest
+(Get-Content python.manifest) -replace '^.*{e2011457-1546-43c5-a5fe-008deee3d3f0}.*$', "<maxversiontested Id=`"10.0.18362.0`"/>`r`n`$0" | Set-Content python.manifest
 
 mt.exe -manifest python.manifest -outputresource:"python\python.exe;#1"
 
 # Enable site.
 
-(Get-Content -Encoding Ascii python/python311._pth) | % {
-    if ($_ -eq "#import site") {
-        "import site"
-    } else {
-        $_
-    }
-} | Set-Content -Encoding Ascii python/python311._pth
+(Get-Content python/python311._pth) -replace '^#import site$', 'import site' | Set-Content python/python311._pth
 
 # Install pip.
 
-curl.exe https://bootstrap.pypa.io/get-pip.py | python\python.exe -
+(Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py).Content | python\python.exe -
 
 # Install py-win32more.
 
