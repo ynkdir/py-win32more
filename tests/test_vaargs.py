b15d62a6dbb4ca980f088288198cdcf18a029aa9
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-09-22 00:17:36 +0900

    Fix test that fails on 32bit

--- tests/test_vaargs.py
+++ tests/test_vaargs.py
@@ -7,7 +7,7 @@ from win32more.Windows.Win32.UI.Shell import wnsprintfW
 class TestVaArgs(unittest.TestCase):
     def test_vaargs(self):
         buf = (Char * 1024)()
-        r = wnsprintfW(buf, len(buf), "hello %s %d %llx", "world", 42, 0x1234)
+        r = wnsprintfW(buf, len(buf), "hello %s %d %x", "world", 42, 0x1234)
         self.assertEqual(len(buf.value), r)
         self.assertEqual(buf.value, "hello world 42 1234")
 
