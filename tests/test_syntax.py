339836ebf14a744b339bebb2a3463ca110415b0d
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-10-13 20:41:58 +0900

    Add test

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -1,4 +1,5 @@
 import unittest
+from ctypes import _CFuncPtr, sizeof
 
 
 class TestSyntax(unittest.TestCase):
@@ -18,3 +19,42 @@ class TestSyntax(unittest.TestCase):
         from win32more.Windows.Win32.Security import SECURITY_NULL_SID_AUTHORITY, SID_IDENTIFIER_AUTHORITY
 
         self.assertIsInstance(SECURITY_NULL_SID_AUTHORITY, SID_IDENTIFIER_AUTHORITY)
+
+    def test_just_import(self):
+        from win32more.Windows.Win32.Foundation import POINT
+
+        self.assertEqual(sizeof(POINT), 8)
+
+    def test_array(self):
+        from win32more.Windows.Win32.Foundation import POINT
+
+        point_array = (POINT * 1)()
+        point_array[0].x = 123
+        self.assertEqual(point_array[0].x, 123)
+
+    def test_circular_referenct_pointer(self):
+        # struct A:
+        #   a: POINTER(A)
+        from win32more.Windows.Win32.System.Kernel import SLIST_ENTRY
+
+        self.assertNotEqual(sizeof(SLIST_ENTRY), 0)
+
+    def test_circular_reference_pointer_direct(self):
+        # struct A:
+        #  b: POINTER(B)
+        # struct B:
+        #  a: A
+        from win32more.Windows.Win32.Devices.WebServicesOnDevices import WSDXML_NODE
+
+        self.assertNotEqual(sizeof(WSDXML_NODE), 0)
+
+    def test_function_pointer(self):
+        from win32more.Windows.Win32.Graphics.Gdi import FONTENUMPROCA
+
+        self.assertTrue(issubclass(FONTENUMPROCA, _CFuncPtr))
+
+    def test_function_pointer_referred_twice(self):
+        from win32more.Windows.Win32.UI.WindowsAndMessaging import WNDCLASSA, WNDCLASSW
+
+        self.assertNotEqual(sizeof(WNDCLASSA), 0)
+        self.assertNotEqual(sizeof(WNDCLASSW), 0)
