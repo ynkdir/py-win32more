22a9c3579b89eb329fc4abec3c6f6a592c5fd7f6
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2023-12-17 18:09:01 +0900

    Add example

---
+++ example/winui-webview.py
@@ -0,0 +1,35 @@
+# https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/winui
+
+from pathlib import Path
+
+from win32more.Microsoft.UI.Xaml import Window
+from win32more.Microsoft.UI.Xaml.Controls import Button, Grid, TextBox, WebView2
+from win32more.Microsoft.UI.Xaml.Markup import XamlReader
+from win32more.Windows.Foundation import Uri
+from win32more.xaml import XamlApplication
+
+
+class App(XamlApplication):
+    def OnLaunched(self, args):
+        win = XamlReader.Load((Path(__file__).parent / "winui-webview.xaml").read_text()).as_(Window)
+
+        grid = win.Content.as_(Grid)
+
+        self.addressBar = grid.FindName("addressBar").as_(TextBox)
+
+        self.myButton = grid.FindName("myButton").as_(Button)
+        self.myButton.add_Click(self.myButton_Click)
+
+        self.MyWebView = grid.FindName("MyWebView").as_(WebView2)
+
+        win.Activate()
+
+    def myButton_Click(self, sender, e):
+        try:
+            targetUri = Uri(self.addressBar.Text)
+            self.MyWebView.Source = targetUri
+        except Exception as e:
+            print(e)
+
+
+XamlApplication.Start(App)
