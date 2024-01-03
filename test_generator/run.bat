cad1e78b0823bd8b352c9b84f2eec080990bdbd5
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-01-03 23:36:28 +0900

    fix test

--- test_generator/run.bat
+++ test_generator/run.bat
@@ -6,13 +6,13 @@ if not exist %WATCHDIR% git init %WATCHDIR%
 hatch run typing win32generator || exit
 hatch run fmt win32generator || exit
 
-del %WATCHDIR%\win32all.py
+rmdir /s /q %WATCHDIR%\win32more
 py -m win32generator --loglevel=DEBUG --package-directory=%WATCHDIR% json\Windows.Win32.json.xz json\WindowsSDK.json.xz json\WindowsAppSDK.json.xz || exit
 
-del %WATCHDIR%\winrtall.py
+del %WATCHDIR%\win32all.py
 py -m win32generator --loglevel=DEBUG --one %WATCHDIR%\win32all.py json\Windows.Win32.json.xz || exit
 
-rmdir /s /q %WATCHDIR%\win32more
+del %WATCHDIR%\winrtall.py
 py -m win32generator --loglevel=DEBUG --one %WATCHDIR%\winrtall.py json\WindowsSDK.json.xz json\WindowsAppSDK.json.xz || exit
 
 rmdir /s /q %WATCHDIR%\win32sel
