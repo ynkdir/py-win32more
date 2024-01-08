a6f954b6f631d2c399b3adfbb8127c39be5f470d
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-01-08 09:52:12 +0900

    refactor

--- win32generator/selector.py
+++ win32generator/selector.py
@@ -37,29 +37,28 @@ class Selector:
             self._ns[td.fullname].append(td)
 
     def _select_match_and_dependencies(self, td: TypeDefinition) -> None:
-        if self._is_match(td.namespace):
-            self._selected.add(td.namespace)
-        elif self._is_match(td.name) or self._is_match(td.fullname):
+        if self._is_match(td.namespace) or self._is_match(td.name) or self._is_match(td.fullname):
             self._selected.add(td.fullname)
-            self._select_dependencies(list(td.enumerate_dependencies()))
+            self._select_dependencies(td.enumerate_dependencies())
         elif td.name == "Apis":
             for fd in td.fields:
                 if self._is_match(fd.name) or self._is_match(f"{td.namespace}.{fd.name}"):
                     self._selected.add(f"{td.namespace}.{fd.name}")
-                    self._select_dependencies(list(fd.enumerate_dependencies()))
+                    self._select_dependencies(fd.enumerate_dependencies())
             for md in td.method_definitions:
                 if self._is_match(md.name) or self._is_match(f"{td.namespace}.{md.name}"):
                     self._selected.add(f"{td.namespace}.{md.name}")
-                    self._select_dependencies(list(md.enumerate_dependencies()))
+                    self._select_dependencies(md.enumerate_dependencies())
         elif td.basetype == "System.Enum":
             if td.is_winrt or not td.custom_attributes.has_scoped_enum():
                 return
             for fd in td.fields[1:]:
                 if self._is_match(fd.name) or self._is_match(f"{td.fullname}.{fd.name}"):
                     self._selected.add(td.fullname)
+                    self._select_dependencies(td.enumerate_dependencies())
                     break
 
-    def _select_dependencies(self, dependencies: list[str]) -> None:
+    def _select_dependencies(self, dependencies: Iterable[str]) -> None:
         for fullname in dependencies:
             if fullname in self._selected:
                 continue
@@ -67,12 +66,10 @@ class Selector:
                 continue
             self._selected.add(fullname)
             for td in self._ns[fullname]:
-                self._select_dependencies(list(td.enumerate_dependencies()))
+                self._select_dependencies(td.enumerate_dependencies())
 
     def _is_selected(self, td: TypeDefinition) -> bool:
-        if td.namespace in self._selected:
-            return True
-        elif td.fullname in self._selected:
+        if td.fullname in self._selected:
             return True
         elif td.name == "Apis":
             td["Fields"] = [fd for fd in td["Fields"] if f"{td['Namespace']}.{fd['Name']}" in self._selected]
