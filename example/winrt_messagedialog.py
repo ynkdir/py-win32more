04bfd76d4d67e39704c4439cf1f2db887d1a8554
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-04-28 10:19:33 +0900

    refactor

--- example/winrt_messagedialog.py
+++ example/winrt_messagedialog.py
@@ -1,10 +1,8 @@
-import asyncio
 import tkinter as tk
-from ctypes import (
-    WinError,
-)
+from ctypes import WinError
 
 from win32more import FAILED
+from win32more.asyncui import async_callback, async_start_runner
 from win32more.Windows.UI.Popups import MessageDialog
 from win32more.Windows.Win32.System.WinRT import (
     RO_INIT_SINGLETHREADED,
@@ -15,13 +13,11 @@ from win32more.Windows.Win32.UI.Shell import IInitializeWithWindow
 
 
 def initialize_with_window(obj, hwnd):
-    ii = IInitializeWithWindow(own=True)
-    hr = obj.QueryInterface(IInitializeWithWindow._iid_, ii)
-    if FAILED(hr):
-        raise WinError(hr)
+    ii = obj.as_(IInitializeWithWindow)
     ii.Initialize(hwnd)
 
 
+@async_callback
 async def winrt_dialog(hwnd):
     dialog = MessageDialog.CreateWithTitle("This is WinRT MessageDialog", "WinRT MessageDialog")
 
@@ -32,37 +28,25 @@ async def winrt_dialog(hwnd):
     print(uicommand, uicommand.Label)
 
 
-async def main2() -> None:
-    is_running = True
-
-    def on_delete():
-        nonlocal is_running
-        is_running = False
-
-    root = tk.Tk()
-    root.protocol("WM_DELETE_WINDOW", on_delete)
-    root.eval("tk::PlaceWindow . center")
-
-    hwnd = root.winfo_id()
-
-    button = tk.Button(root, text="Popup WinRT dialog", command=lambda: asyncio.create_task(winrt_dialog(hwnd)))
-    button.pack(padx=10, pady=10, fill="both", expand=True)
-
-    while is_running:
-        root.after(100, root.quit)
-        root.mainloop()
-        await asyncio.sleep(0)
-
-
-async def main() -> None:
+def main() -> None:
     hr = RoInitialize(RO_INIT_SINGLETHREADED)
     if FAILED(hr):
         raise WinError(hr)
 
-    await main2()
+    root = tk.Tk()
+    root.eval("tk::PlaceWindow . center")
+
+    hwnd = root.winfo_id()
+
+    button = tk.Button(root, text="Popup WinRT dialog", command=lambda: winrt_dialog(hwnd))
+    button.pack(padx=10, pady=10, fill="both", expand=True)
+
+    async_start_runner()
+
+    root.mainloop()
 
     RoUninitialize()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
