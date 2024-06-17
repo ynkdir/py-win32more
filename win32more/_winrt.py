5ee9d3496552a41347d1c3108cfc1e9c0e11dd60
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-06-18 00:38:33 +0900

    Fix unbox_value for array type (#44)

--- win32more/_winrt.py
+++ win32more/_winrt.py
@@ -147,52 +147,126 @@ def unbox_value(value: IInspectable):
 
     property_value = value.as_(IPropertyValue)
 
-    getter = {
-        PropertyType.Empty: lambda: None,
-        PropertyType.UInt8: property_value.GetUInt8,
-        PropertyType.Int16: property_value.GetInt16,
-        PropertyType.UInt16: property_value.GetUInt16,
-        PropertyType.Int32: property_value.GetInt32,
-        PropertyType.UInt32: property_value.GetUInt32,
-        PropertyType.Int64: property_value.GetInt64,
-        PropertyType.UInt64: property_value.GetUInt64,
-        PropertyType.Single: property_value.GetSingle,
-        PropertyType.Double: property_value.GetDouble,
-        PropertyType.Char16: property_value.GetChar16,
-        PropertyType.Boolean: property_value.GetBoolean,
-        PropertyType.String: property_value.GetString,
-        PropertyType.Inspectable: lambda: value,
-        PropertyType.DateTime: property_value.GetDateTime,
-        PropertyType.TimeSpan: property_value.GetTimeSpan,
-        PropertyType.Guid: property_value.GetGuid,
-        PropertyType.Point: property_value.GetPoint,
-        PropertyType.Size: property_value.GetSize,
-        PropertyType.Rect: property_value.GetRect,
-        #PropertyType.OtherType: property_value.GetOtherType,
-        PropertyType.UInt8Array: property_value.GetUInt8Array,
-        PropertyType.Int16Array: property_value.GetInt16Array,
-        PropertyType.UInt16Array: property_value.GetUInt16Array,
-        PropertyType.Int32Array: property_value.GetInt32Array,
-        PropertyType.UInt32Array: property_value.GetUInt32Array,
-        PropertyType.Int64Array: property_value.GetInt64Array,
-        PropertyType.UInt64Array: property_value.GetUInt64Array,
-        PropertyType.SingleArray: property_value.GetSingleArray,
-        PropertyType.DoubleArray: property_value.GetDoubleArray,
-        PropertyType.Char16Array: property_value.GetChar16Array,
-        PropertyType.BooleanArray: property_value.GetBooleanArray,
-        PropertyType.StringArray: property_value.GetStringArray,
-        PropertyType.InspectableArray: property_value.GetInspectableArray,
-        PropertyType.DateTimeArray: property_value.GetDateTimeArray,
-        PropertyType.TimeSpanArray: property_value.GetTimeSpanArray,
-        PropertyType.GuidArray: property_value.GetGuidArray,
-        PropertyType.PointArray: property_value.GetPointArray,
-        PropertyType.SizeArray: property_value.GetSizeArray,
-        PropertyType.RectArray: property_value.GetRectArray,
-        #PropertyType.OtherTypeArray: property_value.GetOtherTypeArray,
-    }.get(property_value.Type)
-    if getter is None:
+    if property_value.Type == PropertyType.Empty:
+        return None
+    elif property_value.Type == PropertyType.UInt8:
+        return property_value.GetUInt8()
+    elif property_value.Type == PropertyType.Int16:
+        return property_value.GetInt16()
+    elif property_value.Type == PropertyType.UInt16:
+        return property_value.GetUInt16()
+    elif property_value.Type == PropertyType.Int32:
+        return property_value.GetInt32()
+    elif property_value.Type == PropertyType.UInt32:
+        return property_value.GetUInt32()
+    elif property_value.Type == PropertyType.Int64:
+        return property_value.GetInt64()
+    elif property_value.Type == PropertyType.UInt64:
+        return property_value.GetUInt64()
+    elif property_value.Type == PropertyType.Single:
+        return property_value.GetSingle()
+    elif property_value.Type == PropertyType.Double:
+        return property_value.GetDouble()
+    elif property_value.Type == PropertyType.Char16:
+        return property_value.GetChar16()
+    elif property_value.Type == PropertyType.Boolean:
+        return property_value.GetBoolean()
+    elif property_value.Type == PropertyType.String:
+        return property_value.GetString()
+    elif property_value.Type == PropertyType.Inspectable:
+        return value
+    elif property_value.Type == PropertyType.DateTime:
+        return property_value.GetDateTime()
+    elif property_value.Type == PropertyType.TimeSpan:
+        return property_value.GetTimeSpan()
+    elif property_value.Type == PropertyType.Guid:
+        return property_value.GetGuid()
+    elif property_value.Type == PropertyType.Point:
+        return property_value.GetPoint()
+    elif property_value.Type == PropertyType.Size:
+        return property_value.GetSize()
+    elif property_value.Type == PropertyType.Rect:
+        return property_value.GetRect()
+    elif property_value.Type == PropertyType.UInt8Array:
+        r = []
+        property_value.GetUInt8Array(r)
+        return r
+    elif property_value.Type == PropertyType.Int16Array:
+        r = []
+        property_value.GetInt16Array(r)
+        return r
+    elif property_value.Type == PropertyType.UInt16Array:
+        r = []
+        property_value.GetUInt16Array(r)
+        return r
+    elif property_value.Type == PropertyType.Int32Array:
+        r = []
+        property_value.GetInt32Array(r)
+        return r
+    elif property_value.Type == PropertyType.UInt32Array:
+        r = []
+        property_value.GetUInt32Array(r)
+        return r
+    elif property_value.Type == PropertyType.Int64Array:
+        r = []
+        property_value.GetInt64Array(r)
+        return r
+    elif property_value.Type == PropertyType.UInt64Array:
+        r = []
+        property_value.GetUInt64Array(r)
+        return r
+    elif property_value.Type == PropertyType.SingleArray:
+        r = []
+        property_value.GetSingleArray(r)
+        return r
+    elif property_value.Type == PropertyType.DoubleArray:
+        r = []
+        property_value.GetDoubleArray(r)
+        return r
+    elif property_value.Type == PropertyType.Char16Array:
+        r = []
+        property_value.GetChar16Array(r)
+        return r
+    elif property_value.Type == PropertyType.BooleanArray:
+        r = []
+        property_value.GetBooleanArray(r)
+        return r
+    elif property_value.Type == PropertyType.StringArray:
+        r = []
+        property_value.GetStringArray(r)
+        return r
+    elif property_value.Type == PropertyType.InspectableArray:
+        r = []
+        property_value.GetInspectableArray(r)
+        return r
+    elif property_value.Type == PropertyType.DateTimeArray:
+        r = []
+        property_value.GetDateTimeArray(r)
+        return r
+    elif property_value.Type == PropertyType.TimeSpanArray:
+        r = []
+        property_value.GetTimeSpanArray(r)
+        return r
+    elif property_value.Type == PropertyType.GuidArray:
+        r = []
+        property_value.GetGuidArray(r)
+        return r
+    elif property_value.Type == PropertyType.PointArray:
+        r = []
+        property_value.GetPointArray(r)
+        return r
+    elif property_value.Type == PropertyType.SizeArray:
+        r = []
+        property_value.GetSizeArray(r)
+        return r
+    elif property_value.Type == PropertyType.RectArray:
+        r = []
+        property_value.GetRectArray(r)
+        return r
+    #elif property_value.Type == PropertyType.OtherType:
+    #elif property_value.Type == PropertyType.OtherTypeArray:
+    else:
         raise NotImplementedError(f"unbox_value: {property_value.Type}")
-    return getter()
 
 
 def generic_get_type_hints(prototype, cls):
