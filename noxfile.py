6b79228ac1b96a3b69daa308d777d53528109a77
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2024-10-13 16:12:56 +0900

    Use nox

---
+++ noxfile.py
@@ -0,0 +1,22 @@
+import nox
+
+
+@nox.session(
+    venv_backend="uv",
+    python=[
+        "3.13",
+        "3.12",
+        "3.11",
+        "3.10",
+        "3.9",
+        "cpython-3.13-windows-x86",
+        "cpython-3.12-windows-x86",
+        "cpython-3.11-windows-x86",
+        "cpython-3.10-windows-x86",
+        "cpython-3.9-windows-x86",
+    ],
+)
+def tests(session):
+    session.install("-e", ".")
+    session.install("pytest")
+    session.run("pytest", *(session.posargs or ["tests"]))
