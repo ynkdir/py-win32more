d7e89e94336eee178d6b7d6f7ebdd6343cb438f1
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-05-04 01:52:48 +0900

    cosmetic change

--- example/getipaddrtable.py
+++ example/getipaddrtable.py
@@ -1,18 +1,21 @@
 import socket
-from ctypes import POINTER, WinError, byref, cast, resize
-from ctypes.wintypes import ULONG
+from ctypes import WinError, create_string_buffer
 
+from win32more import UInt32
 from win32more.Windows.Win32.Foundation import ERROR_INSUFFICIENT_BUFFER, NO_ERROR
 from win32more.Windows.Win32.NetworkManagement.IpHelper import MIB_IPADDRROW_XP, MIB_IPADDRTABLE, GetIpAddrTable
 
-dwSize = ULONG(0)
+dwSize = UInt32(0)
 if (r := GetIpAddrTable(None, dwSize, False)) != ERROR_INSUFFICIENT_BUFFER:
     raise WinError(r)
 
-IpAddrTable = MIB_IPADDRTABLE()
-resize(IpAddrTable, dwSize.value)
+buffer = create_string_buffer(dwSize.value)
+
+IpAddrTable = MIB_IPADDRTABLE.from_buffer(buffer)
 if (r := GetIpAddrTable(IpAddrTable, dwSize, False)) != NO_ERROR:
     raise WinError(r)
 
-for row in cast(byref(IpAddrTable.table), POINTER(MIB_IPADDRROW_XP * IpAddrTable.dwNumEntries)).contents:
+# IpAddrTable.table is flexible array member.
+table = (MIB_IPADDRROW_XP * IpAddrTable.dwNumEntries).from_buffer(buffer, MIB_IPADDRTABLE.table.offset)
+for row in table:
     print(socket.inet_ntoa(row.dwAddr.to_bytes(4, "little")))
