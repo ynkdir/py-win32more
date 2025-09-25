4e10802e56ace97754615fb94c9f5cdacef51864
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-09-25 23:36:20 +0900

    Add .dll for cygwin

--- src/win32more/__init__.py
+++ src/win32more/__init__.py
@@ -3,7 +3,7 @@ from .win32 import *
 # Initialize COM Multithreaded Apartment.
 # Call CoInitializeEx(None, COINIT_APARTMENTTHREADED) explicitly for Single-Threaded Apartment.
 cookie = VoidPtr()
-hr = windll["ole32"].CoIncrementMTAUsage(pointer(cookie))
+hr = windll["ole32.dll"].CoIncrementMTAUsage(pointer(cookie))
 if FAILED(hr):
     raise WinError(hr)
 del hr
