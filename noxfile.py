16bb55ae1b5cae18a75bc1c62645425549ca36c2
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-10-22 21:10:53 +0900

    Run test with local package

--- noxfile.py
+++ noxfile.py
@@ -1,18 +1,7 @@
 import nox
 
 
-@nox.session(
-    venv_backend="uv",
-    python=[
-        "3.14",
-        "3.13",
-        "3.12",
-        "3.11",
-        "3.10",
-        "3.9",
-    ],
-)
+@nox.session(venv_backend="uv", python=["3.14", "3.13", "3.12", "3.11", "3.10", "3.9"])
 def tests(session):
-    session.install(".")
-    session.install("pytest")
+    session.install("-r", "test_requirements.txt", "--reinstall-package", "win32more")
     session.run("pytest", *(session.posargs or ["tests"]))
