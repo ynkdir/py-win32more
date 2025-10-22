8cbf331447b41825f3fc841998af229a55f3fc99
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-10-22 20:56:13 +0900

    Drop 32bit from noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -10,12 +10,6 @@ import nox
         "3.11",
         "3.10",
         "3.9",
-        "cpython-3.14-windows-x86",
-        "cpython-3.13-windows-x86",
-        "cpython-3.12-windows-x86",
-        "cpython-3.11-windows-x86",
-        "cpython-3.10-windows-x86",
-        "cpython-3.9-windows-x86",
     ],
 )
 def tests(session):
