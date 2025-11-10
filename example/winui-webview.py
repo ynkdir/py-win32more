59adb5d0640f974289312e56183b1af663ac270f
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-11-10 20:44:24 +0900

    Fix example

--- example/winui-webview.py
+++ example/winui-webview.py
@@ -1,29 +1,42 @@
 # https://learn.microsoft.com/en-us/microsoft-edge/webview2/get-started/winui
 
-from pathlib import Path
 
-from win32more.Microsoft.UI.Xaml import Window
-from win32more.Microsoft.UI.Xaml.Controls import Button, Grid, TextBox, WebView2
-from win32more.Microsoft.UI.Xaml.Markup import XamlReader
 from win32more.Windows.Foundation import Uri
+from win32more.winui3 import XamlApplication, XamlLoader
 
-from win32more.winui3 import XamlApplication
+xaml = """
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+    xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+    xmlns:controls="using:Microsoft.UI.Xaml.Controls"
+    mc:Ignorable="d">
+
+    <Grid>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="Auto"/>
+            <RowDefinition Height="*"/>
+        </Grid.RowDefinitions>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="*"/>
+            <ColumnDefinition Width="Auto"/>
+        </Grid.ColumnDefinitions>
+
+        <TextBox x:Name="addressBar" Grid.Column="0"/>
+        <Button x:Name="myButton" Grid.Column="1" Click="myButton_Click">Go</Button>
+
+        <controls:WebView2 x:Name="MyWebView" Grid.Row="1" Grid.ColumnSpan="2"
+            Source="https://www.microsoft.com" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"/>
+    </Grid>
+</Window>
+"""
 
 
 class App(XamlApplication):
     def OnLaunched(self, args):
-        win = XamlReader.Load((Path(__file__).parent / "winui-webview.xaml").read_text()).as_(Window)
-
-        grid = win.Content.as_(Grid)
-
-        self.addressBar = grid.FindName("addressBar").as_(TextBox)
-
-        self.myButton = grid.FindName("myButton").as_(Button)
-        self.myButton.add_Click(self.myButton_Click)
-
-        self.MyWebView = grid.FindName("MyWebView").as_(WebView2)
-
-        win.Activate()
+        self._window = XamlLoader.Load(self, xaml)
+        self._window.Activate()
 
     def myButton_Click(self, sender, e):
         try:
@@ -33,4 +46,5 @@ class App(XamlApplication):
             print(e)
 
 
-XamlApplication.Start(App)
+if __name__ == "__main__":
+    XamlApplication.Start(App)
