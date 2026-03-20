bec3fad7106d5b9151785bc873099396f638dc24
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-03-21 03:38:01 +0900

    Fix script

--- scripts/apisearch.py
+++ scripts/apisearch.py
@@ -3,7 +3,10 @@
 # ///
 
 import argparse
+import asyncio
+import concurrent.futures
 import io
+import itertools
 import json
 import logging
 import urllib.request
@@ -12,6 +15,7 @@ from collections.abc import Iterable
 from pathlib import Path
 from typing import Any
 
+from win32more import box_value
 from win32more.Microsoft.UI.Xaml import FocusState
 from win32more.winui3 import XamlApplication, XamlLoader
 
@@ -29,16 +33,6 @@ logger = logging.getLogger(__name__)
 
 
 class App(XamlApplication):
-    def __init__(self):
-        super().__init__()
-
-        if APILIST_CACHE.exists():
-            self._apilist = json.loads(APILIST_CACHE.read_text())
-        else:
-            self._apilist = ApiListGenerator().generate()
-            logger.info(f"writing cache file {APILIST_CACHE}")
-            APILIST_CACHE.write_text(json.dumps(self._apilist))
-
     def OnLaunched(self, args):
         self.Window = XamlLoader.Load(
             self,
@@ -68,43 +62,120 @@ class App(XamlApplication):
         self.Window.Title = "Api Search"
         self.Window.Activate()
 
-    def TextBlock1_TextChanged(self, sender, e):
-        query = self.TextBlock1.Text.lower()
-        items = self.ListView1.Items
-        items.Clear()
+        self._ready = False
+        self._search_engine = SearchEngine()
+        self._current_search_task = None
 
-        if query == "":
+        asyncio.create_task(self._load())
+
+    async def _load(self) -> None:
+        self.ListView1.Items.Append(
+            XamlLoader.Load(
+                self,
+                """<TextBlock xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">loading</TextBlock>""",
+            )
+        )
+        self.ListView1.Items.Append(
+            XamlLoader.Load(
+                self,
+                """<ProgressBar xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" IsIndeterminate="True" />""",
+            )
+        )
+
+        await asyncio.to_thread(self._search_engine.load)
+
+        self._ready = True
+
+        if self.TextBlock1.Text != "":
+            await self._search()
             return
 
-        count = 0
-        for api in self._apilist:
-            if query in api["FQN"].lower():
-                items.Append(
-                    XamlLoader.Load(
-                        self,
-                        f'<TextBlock xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">{self.format_api(api)}</TextBlock>',
-                    )
-                )
-                count += 1
-                if count == LIST_MAX:
-                    items.Append(f"... (over {LIST_MAX})")
-                    break
+        self.ListView1.Items.Clear()
+        self.ListView1.Items.Append(
+            XamlLoader.Load(
+                self,
+                """<TextBlock xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">ready</TextBlock>""",
+            )
+        )
+
+    async def _search(self) -> None:
+        if not self._ready:
+            return
+
+        if self._current_search_task is not None:
+            self._current_search_task.cancel()
+
+        self._current_search_task = asyncio.current_task()
+
+        query = self.TextBlock1.Text
+        items = self.ListView1.Items
+
+        # debouncing
+        try:
+            await asyncio.sleep(0.3)
+        except asyncio.CancelledError:
+            return
+
+        if query == "":
+            items.Clear()
+            return
+
+        try:
+            matched_api = await asyncio.to_thread(self._search_engine.search, query)
+        except asyncio.CancelledError:
+            return
+
+        r = []
+        for i, api in enumerate(matched_api):
+            r.append(self.make_item(api))
+            if i == LIST_MAX:
+                r.append(box_value(f"... (over {LIST_MAX})"))
+                break
+        items.ReplaceAll(r)
+
+        self._current_search_task = None
+
+    def make_item(self, api: Any) -> object:
+        if api["Doc"] is None:
+            link = ""
+        else:
+            link = f"""<Hyperlink NavigateUri="{api["Doc"]}">doc</Hyperlink>"""
+        if "Method" in api:
+            item = f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Class']}</Run>.<Run Foreground='Maroon'>{api['Method']}</Run> {link}"
+        elif "Field" in api:
+            item = f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Class']}</Run>.<Run Foreground='Maroon'>{api['Field']}</Run> {link}"
+        else:
+            item = f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Name']}</Run> {link}"
+        xaml = f'<TextBlock xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">{item}</TextBlock>'
+        return XamlLoader.Load(self, xaml)
+
+    async def TextBlock1_TextChanged(self, sender, e):
+        await self._search()
 
     def TextBlock1_Loaded(self, sender, e):
         self.TextBlock1.Focus(FocusState.Programmatic)
 
-    def format_api(self, api: Any) -> str:
-        link = self.format_doc(api)
-        if "Method" in api:
-            return f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Class']}</Run>.<Run Foreground='Maroon'>{api['Method']}</Run> {link}"
-        elif "Field" in api:
-            return f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Class']}</Run>.<Run Foreground='Maroon'>{api['Field']}</Run> {link}"
-        return f"{api['Namespace']}.<Run Foreground='Tomato'>{api['Name']}</Run> {link}"
 
-    def format_doc(self, api: Any) -> str:
-        if api["Doc"] is None:
-            return ""
-        return f"""<Hyperlink NavigateUri="{api["Doc"]}">doc</Hyperlink>"""
+class SearchEngine:
+    def __init__(self):
+        self._apilist = []
+
+    def load(self) -> None:
+        if APILIST_CACHE.exists():
+            self._apilist = json.loads(APILIST_CACHE.read_text())
+        else:
+            meta = MetadataLoader().load()
+            self._apilist = ApiListGenerator().generate(meta)
+            logger.info(f"writing cache file {APILIST_CACHE}")
+            APILIST_CACHE.write_text(json.dumps(self._apilist))
+
+    def search(self, query: str) -> list[Any]:
+        r = []
+        query = query.lower()
+        for api in self._apilist:
+            if query in api["FQN_lower"]:
+                r.append(api)
+        return r
 
 
 class MetadataLoader:
@@ -118,38 +189,41 @@ class MetadataLoader:
         file = io.BytesIO(data)
         return zipfile.ZipFile(file)
 
-    def load_files(self, metadata_files: list[zipfile.ZipPath]) -> Any:
-        js = []
-        for zfile in metadata_files:
-            for zname in zfile.namelist():
-                zpath = zipfile.Path(zfile, zname)
-                js.extend(json.loads(zpath.read_text()))
-        return js
+    def download_metadata(self, url: str) -> Iterable[Any]:
+        zfile = self.download_zip(url)
+        for zname in zfile.namelist():
+            zpath = zipfile.Path(zfile, zname)
+            yield from json.loads(zpath.read_text())
 
-    def load(self):
-        meta = self.load_files([self.download_zip(url) for url in METADATA_URLS])
-        return list(self.preprocess(meta))
+    def load(self) -> list[Any]:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            return list(
+                itertools.chain.from_iterable(
+                    executor.map(
+                        lambda url: [td for td in self.download_metadata(url) if self.is_public(td)], METADATA_URLS
+                    )
+                )
+            )
 
-    def preprocess(self, meta: Any) -> Iterable[Any]:
-        for td in meta:
-            if "WindowsRuntime" in td["Attributes"]:
-                if td["Namespace"] == "" or td["BaseType"] == "System.Attribute":
-                    continue
-            else:
-                if td["Namespace"] == "" or "Public" not in td["Attributes"]:
-                    continue
-            yield td
-
-        return meta
+    def is_public(self, td: Any) -> bool:
+        is_winrt = "WindowsRuntime" in td["Attributes"]
+        if is_winrt:
+            return td["Namespace"] != "" and td["BaseType"] != "System.Attribute"
+        else:
+            return td["Namespace"] != "" and "Public" in td["Attributes"]
 
 
 class ApiListGenerator:
-    def generate(self):
-        meta = MetadataLoader().load()
+    def generate(self, meta: list[Any]):
         logger.info("generating api list")
-        return list(self.enumerate_name(meta))
+        return list(self.enumerate_name_with_lower_fqn(meta))
 
-    def enumerate_name(self, meta: Any) -> Iterable[str]:
+    def enumerate_name_with_lower_fqn(self, meta: list[Any]) -> Iterable[str]:
+        for api in self.enumerate_name(meta):
+            api["FQN_lower"] = api["FQN"].lower()
+            yield api
+
+    def enumerate_name(self, meta: list[Any]) -> Iterable[str]:
         for td in meta:
             if td["Name"] == "Apis":
                 for fd in td["Fields"]:
