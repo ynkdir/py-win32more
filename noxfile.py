c00ce9fed9efa8ccdee5228a3517e10a627aa0a5
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-05-08 23:55:22 +0900

    Add python3.15

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(venv_backend="uv", python=["3.14", "3.13", "3.12", "3.11", "3.10", "3.9"])
+@nox.session(venv_backend="uv", python=["3.15", "3.14", "3.13", "3.12", "3.11", "3.10", "3.9"])
 def tests(session):
     session.install("-r", "test_requirements.txt")
     session.install("packages/core", "packages/appsdk")  # force reinstall
