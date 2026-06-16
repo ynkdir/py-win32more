f35b4f6cfaf1a47c9688886d8f1aaaeff09c5c1a
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-06-17 00:59:04 +0900

    Fix example

--- example/appsdk_notification.py
+++ example/appsdk_notification.py
@@ -1,13 +1,65 @@
+# NOTE: This program add registry key
+#
+# [HKEY_CURRENT_USER\Software\Classes\AppUserModelId\win32more-example-appsdk-notification]
+# "CustomActivator"="{CF597ACA-A2FB-4F02-BD03-FF7351D44055}"
+# "DisplayName"="appsdk_notification.py"
+# "IconUri"="...\icon1.png"
+#
+# [HKEY_CURRENT_USER\Software\Classes\CLSID\{CF597ACA-A2FB-4F02-BD03-FF7351D44055}\LocalServer32]
+# @="\"C:\path\to\python.exe\" \"...\apsdk_notification.py" ----AppNotificationActivated:"
+
+import sys
+import time
+import winreg
+from pathlib import Path
+
 from win32more.Microsoft.Windows.AppNotifications import AppNotificationManager
 from win32more.Microsoft.Windows.AppNotifications.Builder import AppNotificationBuilder
+from win32more.Windows.Foundation import Uri
+from win32more.Windows.Win32.UI.Shell import SetCurrentProcessExplicitAppUserModelID
+
+AUMID = "win32more-example-appsdk-notification"
+CUSTOM_ACTIVATOR = "{CF597ACA-A2FB-4F02-BD03-FF7351D44055}"
+DISPLAY_NAME = "appsdk_notification.py"
+KEY_APP_USER_MODEL_ID = "Software\\Classes\\AppUserModelId"
+KEY_CLSID = "Software\\Classes\\CLSID"
 
 
-def main() -> None:
+def is_registered(aumid):
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, f"{KEY_APP_USER_MODEL_ID}\\{aumid}"):
+            pass
+        return True
+    except OSError:
+        pass
+    return False
+
+
+def register_app_user_model_id_and_custom_activator(aumid, custom_activator, cmd):
+    with winreg.CreateKey(winreg.HKEY_CURRENT_USER, f"{KEY_APP_USER_MODEL_ID}\\{aumid}") as key:
+        winreg.SetValueEx(key, "CustomActivator", 0, winreg.REG_SZ, custom_activator)
+    with winreg.CreateKey(winreg.HKEY_CURRENT_USER, f"{KEY_CLSID}\\{custom_activator}\\LocalServer32") as key:
+        winreg.SetValueEx(key, "", 0, winreg.REG_SZ, cmd)
+
+
+# Setup AppUserModelId and CustomActivator registry, so that toast notification can run custom command line.
+def setup_aumid():
+    SetCurrentProcessExplicitAppUserModelID(AUMID)
+
+    if is_registered(AUMID):
+        return
+
+    cmd = f'"{sys.executable}" "{__file__}" ----AppNotificationActivated:'
+    register_app_user_model_id_and_custom_activator(AUMID, CUSTOM_ACTIVATOR, cmd)
+
+
+def notify():
+    setup_aumid()
+
     notification_manager = AppNotificationManager.Default
 
-    notification_manager.Register()
+    notification_manager.Register(DISPLAY_NAME, Uri(str(Path(__file__).with_name("icon1.png"))))
 
-    # How to set title?
     notification = AppNotificationBuilder().AddText("title").AddText("message").BuildNotification()
 
     notification_manager.Show(notification)
@@ -16,10 +68,22 @@ def main() -> None:
     #   "After calling Unregister, any subsequent calls to invoke the Notification by the user would launch a new process"
     # New process is executed as:
     #   /path/to/python.exe ----AppNotificationActivated: -Embedding
-    # where python.exe is real executable, not venv/Scripts/python.exe even if you are using venv.
-    # (because venv/Scripts/python.exe fork system installed python.exe)
-    # And python.exe is immediately exit with "unknown option" error.
     notification_manager.Unregister()
+
+
+def notification_activated():
+    print(sys.argv)
+    print("AppNotificationActivated!")
+    for i in range(5, 1, -1):
+        print(i)
+        time.sleep(1)
+
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == "----AppNotificationActivated:":
+        notification_activated()
+    else:
+        notify()
 
 
 if __name__ == "__main__":
