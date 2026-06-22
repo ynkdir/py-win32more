bb4380569287643404c7cefd0e820adf21c6665d
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-06-22 22:55:17 +0900

    Fix example

--- example/appsdk_notification.py
+++ example/appsdk_notification.py
@@ -8,13 +8,14 @@
 # [HKEY_CURRENT_USER\Software\Classes\CLSID\{CF597ACA-A2FB-4F02-BD03-FF7351D44055}\LocalServer32]
 # @="\"C:\path\to\python.exe\" \"...\appsdk_notification.py\" ----AppNotificationActivated:"
 
+import os
+import signal
 import sys
-import time
 import winreg
 from pathlib import Path
 
 from win32more.Microsoft.Windows.AppLifecycle import AppInstance, ExtendedActivationKind
-from win32more.Microsoft.Windows.AppNotifications import AppNotificationManager, IAppNotificationActivatedEventArgs
+from win32more.Microsoft.Windows.AppNotifications import AppNotificationActivatedEventArgs, AppNotificationManager
 from win32more.Microsoft.Windows.AppNotifications.Builder import AppNotificationBuilder, AppNotificationButton
 from win32more.Windows.Foundation import Uri
 from win32more.Windows.Win32.UI.Shell import SetCurrentProcessExplicitAppUserModelID
@@ -54,20 +55,27 @@ def notify(notification_manager):
     notification_manager.Show(notification)
 
 
-def notification_activated(args):
+def notification_invoked(sender, args):
+    print("NotificationInvoked!")
+    print("    Arguments=", dict(args.Arguments))
+    print("    UserInput=", dict(args.UserInput))
+    # exit input() in main thread
+    os.kill(os.getpid(), signal.SIGINT)
+
+
+def notification_activated(sender, args):
     print(sys.argv)
     print("AppNotificationActivated!")
-    print("    Argument=", args.Argument)
+    print("    Arguments=", dict(args.Arguments))
     print("    UserInput=", dict(args.UserInput))
-    for i in range(5, 1, -1):
-        print(i)
-        time.sleep(1)
 
 
 def main():
     setup_aumid()
 
     notification_manager = AppNotificationManager.Default
+
+    notification_manager.NotificationInvoked += notification_invoked
 
     notification_manager.Register(DISPLAY_NAME, ICON_URI)
 
@@ -76,9 +84,14 @@ def main():
     if args.Kind == ExtendedActivationKind.Launch:
         notify(notification_manager)
     elif args.Kind == ExtendedActivationKind.AppNotification:
-        notification_activated(args.Data.as_(IAppNotificationActivatedEventArgs))
+        notification_activated(notification_manager, args.Data.as_(AppNotificationActivatedEventArgs))
     else:
         raise RuntimeError("Unexpected kind:", args.Kind)
+
+    try:
+        input("Press ENTER to exit")
+    except KeyboardInterrupt:
+        pass
 
     # Documentation says:
     #   "After calling Unregister, any subsequent calls to invoke the Notification by the user would launch a new process"
