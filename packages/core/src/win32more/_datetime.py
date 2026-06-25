46116b64f37a64ed44c40cf36748197a85e89a41
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-06-25 23:08:22 +0900

    Fix localtzinfo

--- packages/core/src/win32more/_datetime.py
+++ packages/core/src/win32more/_datetime.py
@@ -11,20 +11,24 @@ FILETIME_EPOCH = datetime(1601, 1, 1, tzinfo=timezone.utc)
 def datetime_to_winrt(value: datetime) -> DateTime:
     # ensure aware datetime
     if value.tzinfo is None:
-        value = value.replace(tzinfo=localtzinfo())
+        value = value.replace(tzinfo=localtzinfo(value))
     return DateTime((value - FILETIME_EPOCH) // timedelta(microseconds=1) * 10)
 
 
 # nanoseconds are dropped
+# FIXME: should return utc?
 def datetime_from_winrt(value: DateTime) -> datetime:
     utc = FILETIME_EPOCH + timedelta(microseconds=value.UniversalTime // 10)
-    local = localtzinfo()
+    local = localtzinfo(utc)
     return (utc + local.utcoffset(None)).replace(tzinfo=local)
 
 
 # <3.15: astimezone() raises OSError for pre-EPOCH datetime
-def localtzinfo():
-    return datetime(2006, 1, 2).astimezone().tzinfo
+def localtzinfo(dt: datetime) -> timezone:
+    try:
+        return dt.astimezone().tzinfo
+    except OSError:
+        return dt.replace(year=1971).astimezone().tzinfo
 
 
 def timedelta_to_winrt(value: timedelta) -> TimeSpan:
