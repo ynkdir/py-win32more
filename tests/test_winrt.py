a4fb9fa4ab445a9ccdf70779b4944238b9066bde
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-09-09 21:31:19 +0900

    Add test

--- tests/test_winrt.py
+++ tests/test_winrt.py
@@ -69,6 +69,7 @@ from win32more.Windows.Foundation.Collections import (
     IVector,
     IVectorView,
     StringMap,
+    IObservableVector,
 )
 from win32more.Windows.Globalization import Calendar
 from win32more.Windows.Storage import FileIO, PathIO, StorageFile
@@ -1174,6 +1175,12 @@ class TestWinrt(unittest.TestCase):
         self.assertEqual(received, [d, t, d])
         for value, expected in zip(received, [datetime, timedelta, datetime]):
             self.assertIsInstance(value, expected)
+
+    def test_observable_vector_has_sequence_protocol(self):
+        ov = Vector[Int32]([1, 2, 3]).as_(IObservableVector[Int32])
+        self.assertEqual(len(ov), 3)
+        self.assertEqual([v for v in ov], [1, 2, 3])
+
 
 
 if __name__ == "__main__":
